88ad7a147b9b0503af4ba7c612819fb055acd0ec
Ajay Kumar <ajay.kumar@urbanft.com>
2025-06-30 22:42:54 +0530

    Fixed publish command

--- opencv/java/src/org/opencv/android/CameraBridgeViewBase.java
+++ opencv/java/src/org/opencv/android/CameraBridgeViewBase.java
@@ -1,5 +1,12 @@
 package org.opencv.android;
 
+import java.util.List;
+
+import org.opencv.BuildConfig;
+import org.opencv.R;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -9,17 +16,12 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
-import org.opencv.R;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
-
-import java.util.List;
 
 /**
  * This is a basic class, implementing the interaction with Camera and OpenCV library.
